47b53f338c12cd6cf1db1c49633373a54f77682e
Tomas Korenko <radixxko@gmail.com>
2018-10-31 12:25:41 +0100

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# liqd-querystring
+
+[![NPM version](https://img.shields.io/npm/v/liqd-querystring.svg)](https://www.npmjs.com/package/liqd-querystring)
+[![Build Status](https://travis-ci.org/radixxko/liqd-querystring.svg?branch=master)](https://travis-ci.org/radixxko/liqd-querystring)
+[![Coverage Status](https://coveralls.io/repos/github/radixxko/liqd-querystring/badge.svg?branch=master)](https://coveralls.io/github/radixxko/liqd-querystring?branch=master)
+[![NPM downloads](https://img.shields.io/npm/dm/liqd-querystring.svg)](https://www.npmjs.com/package/liqd-querystring)
+[![Known Vulnerabilities](https://snyk.io/test/github/radixxko/liqd-querystring/badge.svg?targetFile=package.json)](https://snyk.io/test/github/radixxko/liqd-querystring?targetFile=package.json)
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
---
+++ test/test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const assert = require('assert');
+const QS = require('../lib/querystring.js');
+
+describe( 'Tests', ( done ) =>
+{
+    it('should parse query string', function()
+	{
+        assert.deepStrictEqual( QS.parse( 'foo=bar' ), { foo: 'bar' });
+        assert.deepStrictEqual( QS.parse( 'foo=bar&bar=foo' ), { foo: 'bar', bar: 'foo' });
+    });
+
+    it('should parse query starting with &', function()
+	{
+        assert.deepStrictEqual( QS.parse( 'foo=bar&bar=foo&' ), { foo: 'bar', bar: 'foo' });
+        assert.deepStrictEqual( QS.parse( '&foo=bar&bar=foo' ), { foo: 'bar', bar: 'foo' });
+        assert.deepStrictEqual( QS.parse( '&foo=bar&bar=foo&' ), { foo: 'bar', bar: 'foo' });
+    });
+
+    it('should parse empty query', function()
+	{
+        assert.deepStrictEqual( QS.parse('&'), {});
+        assert.deepStrictEqual( QS.parse('='), {});
+        assert.deepStrictEqual( QS.parse('&=&='), {});
+    });
+
+    it('should parse empty values', function()
+	{
+        assert.deepStrictEqual( QS.parse('foo'), { foo: null });
+        assert.deepStrictEqual( QS.parse('foo&bar'), { foo: null, bar: null });
+        assert.deepStrictEqual( QS.parse('foo&bar&'), { foo: null, bar: null });
+        assert.deepStrictEqual( QS.parse('foo='), { foo: '' });
+        assert.deepStrictEqual( QS.parse('foo&'), { foo: null });
+        assert.deepStrictEqual( QS.parse('foo=&'), { foo: '' });
+        assert.deepStrictEqual( QS.parse('foo=&bar='), { foo: '', bar: '' });
+        assert.deepStrictEqual( QS.parse('foo=&bar=&'), { foo: '', bar: '' });
+        assert.deepStrictEqual( QS.parse('foo=&bar&'), { foo: '', bar: null });
+        assert.deepStrictEqual( QS.parse('foo=&bar=foo'), { foo: '', bar: 'foo' });
+        assert.deepStrictEqual( QS.parse('foo&bar=foo'), { foo: null, bar: 'foo' });
+    });
+
+    it('should parse arrays', function()
+	{
+        assert.deepStrictEqual( QS.parse('foo=a&foo=b'), { foo: [ 'a', 'b' ] });
+        assert.deepStrictEqual( QS.parse('foo[]=a&foo[]=b'), { foo: [ 'a', 'b' ] });
+        assert.deepStrictEqual( QS.parse('foo[]=a&foo[1]=b'), { foo: [ 'a', 'b' ] });
+        assert.deepStrictEqual( QS.parse('foo[0]=a&foo[1]=b'), { foo: [ 'a', 'b' ] });
+        assert.deepStrictEqual( QS.parse('foo[1]=a&foo[0]=b'), { foo: [ 'b', 'a' ] });
+        assert.deepStrictEqual( QS.parse('foo[bar]=a&foo[bar]=b'), { foo: { bar: [ 'a', 'b' ] }});
+        assert.deepStrictEqual( QS.parse('foo[bar][]=a&foo[bar][]=b'), { foo: { bar: [ 'a', 'b' ] }});
+        assert.deepStrictEqual( QS.parse('foo[bar][0]=a&foo[bar][1]=b'), { foo: { bar: [ 'a', 'b' ] }});
+        assert.deepStrictEqual( QS.parse('foo[bar][1]=a&foo[bar][0]=b'), { foo: { bar: [ 'b', 'a' ] }});
+        assert.deepStrictEqual( QS.parse('foo[bar][0]=a&foo[bar]=b'), { foo: { bar: [ 'a', 'b' ]}});
+        assert.deepStrictEqual( QS.parse('foo[bar][foo]=a&foo[bar]=b'), { foo: { bar: { foo: 'a', '0' : 'b' }}});
+    });
+
+    it('should parse objects', function()
+	{
+        assert.deepStrictEqual( QS.parse('foo[bar]=a'), { foo: { bar: 'a' }});
+        assert.deepStrictEqual( QS.parse('foo[bar]=a&foo[]=b'), { foo: { bar: 'a', '0': 'b' }});
+        assert.deepStrictEqual( QS.parse('foo[bar]=a&foo[]=b&foo[]=c'), { foo: { bar: 'a', '0': 'b', '1': 'c' }});
+        assert.deepStrictEqual( QS.parse('foo[]=b&foo[bar]=a&foo[]=c'), { foo: { bar: 'a', '0': 'b', '1': 'c' }});
+    });
+
+    it('should decode keys and values', function()
+	{
+        assert.deepStrictEqual( QS.parse('foo+bar=bar+foo'), { 'foo bar': 'bar foo' });
+        assert.deepStrictEqual( QS.parse('foo+bar'), { 'foo bar': null });
+        assert.deepStrictEqual( QS.parse('foo+bar='), { 'foo bar': '' });
+        assert.deepStrictEqual( QS.parse('foo+bar=+'), { 'foo bar': ' ' });
+    });
+});
